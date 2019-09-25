f29b589e6668a9c394553f41ddb10685eac720a3
MrFreyd <antosha.konkov@mail.ru>
2019-09-25 16:43:28 +0700

    header mobile development

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var click = 0;
+var element = document.getElementById('click');
+element.onclick = function(){
+	alert(1);
+}
+
+
+
+
+
+/*
+$('.hiden').on('click',function(){
+click++
+
+$(window).resize(function() { 
+	if(document.body.clientWidth>600){
+		$('.hiden-menu').hide();
+		click = 0
+	}
+})
+	
+	$('.hiden-menu').show();
+	
+	if(click == 2){
+		$('.hiden-menu').hide();
+		click = 0;
+	}
+	
+	
+})
+*/
